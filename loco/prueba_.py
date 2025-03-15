24578b771ab923f624a36e536cc154e02db2772d
CamiloDlrM <zinbak78@gmail.com>
2025-03-15 07:17:01 -0500

    comprobacion de conexion a bd por medio de python

--- loco/prueba_.py
+++ loco/prueba_.py
@@ -1,4 +1,19 @@
 import numpy as np
 from flask import Flask
 
+import psycopg2
+
+try:
+    # Intentar conectar a la base de datos
+    conn = psycopg2.connect(
+        dbname="mydatabase_prueba",
+        user="myuser",
+        password="mypassword",
+        host="database",
+        port="5432"
+    )
+    print("Conexión exitosa a la base de datos")
+    conn.close()
+except Exception as e:
+    print(f"Error al conectar: {e}")
 
